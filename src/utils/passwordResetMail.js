944202bc9f8cd4c0571521a950f821dee46d58b4
nihaln0077 <nihaln0077@gmail.com>
2021-10-03 14:23:06 +0530

    sendGrid implemented

--- src/utils/passwordResetMail.js
+++ src/utils/passwordResetMail.js
@@ -1,37 +1,29 @@
-const nodemailer = require("nodemailer");
-const smtpPool = require("nodemailer-smtp-pool");
+const sgMail = require("@sendgrid/mail");
 
 const randomOtp = require("./randomOtp");
 require("dotenv").config();
 
-const transporter = nodemailer.createTransport(smtpPool({
-    service: 'gmail',
-    auth: {
-      user: process.env.EMAIL,
-      pass: process.env.EMAIL_PASS
-    },
-    maxConnections: 5,
-    maxMessages: 10
-  }));
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 const passwordResetMail = (email) => {
     return new Promise((resolve, reject) => {
         const otp = randomOtp();
-
-        const mailOptions = {
-            from: "nihaln0077@gmail.com",
-            to: email,
-            subject: "Sending Email using Node.js",
-            text: "That was easy! " + otp,
+        const msg = {
+            to: email, // Change to your recipient
+            from: "nihaln0066@gmail.com", // Change to your verified sender
+            subject: "Seen Web",
+            text: "Your Seen web password reset otp.",
+            html: `<p>You 6 digit OTP is <b>${otp}</b>. Do not share with anyone.</p>`,
         };
 
-        transporter.sendMail(mailOptions, function (err, info) {
-            if (err) {
-                reject(err);
-            } else {
+        sgMail
+            .send(msg)
+            .then(() => {
                 resolve(otp);
-            }
-        });
+            })
+            .catch((error) => {
+                reject(error);
+            });
     });
 };
 
